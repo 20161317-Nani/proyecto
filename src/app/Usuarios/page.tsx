db9038455c9ea50ec0e032f02758812932bfd0f9
Nani1802 <naniramirezjacinto@gmail.com>
2026-03-22 00:42:07 -0600

    usuarios

---
+++ src/app/Usuarios/page.tsx
@@ -0,0 +1,6 @@
+// app/page.tsx
+import Usuarios from '@/components/Usuarios/usuarios'; // Ajusta la ruta según tu proyecto
+
+export default function Page() {
+  return <Usuarios />; // Aquí solo llamamos a tu componente
+}
